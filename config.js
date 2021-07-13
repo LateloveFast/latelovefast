40f82027cb93da03f6cb33cadbd6c085467193e3
LateloveFast <87347820+LateloveFast@users.noreply.github.com>
2021-07-13 09:23:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'Babe à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là iu bé nhưng nhiều nhiều <33',
+    btnNo: 'Không, iu em một xíu thôi :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại iu bé <3',
+    btnReply: 'Gửi cho bé iu <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    mess: 'Bé biết mò 🥰. Yêu anh nhìu nhìu 😘😘',
+    messDesc: 'Lên HN cho bé cưỡi anh nha.',
+    btnAccept: 'Okiiiii lun nha bé <3',
+    messLink: 'https://www.facebook.com/thwphuongg' 
 }
