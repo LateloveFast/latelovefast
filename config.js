3212269e16c9bd56317efb75febe64f23ab38b2c
LateloveFast <87347820+LateloveFast@users.noreply.github.com>
2021-07-13 13:17:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho bé <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Bé biết mà 🥰. Yêu anh iu nhìu nhìu 😘😘',
-    messDesc: 'Anh hứa lên HN gì anh cũng chiều đó nên cho bé cưỡi nhaaaa',
+    messDesc: 'Anh hứa lên HN gì anh cũng chiều đó nên cho bé cưỡi nhaaaaa',
     btnAccept: 'Okiiiii lun bé iu cụa anh<3',
     messLink: 'https://www.facebook.com/nomevn/posts/2716743091911620' 
 }
